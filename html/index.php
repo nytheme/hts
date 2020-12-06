1e8869ee8688b7613e4c59f8327ed422cdb5d713
nytheme <arkznm@yahoo.co.jp>
2020-12-06 14:20:27 +0900

    first commit

--- html/index.php
+++ html/index.php
@@ -6,44 +6,44 @@ $dsn = 'mysql:host='.$host_name.';dbname='.$db_name.';charset=utf8';
 $db_user = 'root';
 $db_pass = 'secret';
 $table_name = 'test';
-$keyColumn_name = 'name';		//値があるか検索したいカラム名を代入
-$valueColumn_name = 'value';	//値を変更したいカラム名を代入
+$keyColumn_name = 'name';//値があるか検索したいカラム名を代入
+$valueColumn_name = 'value';//値を変更したいカラム名を代入
 
 try {
-	$pdo = new PDO($dsn, $db_user, $db_pass);
-	// echo 'データベース接続成功' . PHP_EOL;
+$pdo = new PDO($dsn, $db_user, $db_pass);
+// echo 'データベース接続成功' . PHP_EOL;
 
-	if (isset($_GET['key'])) {
-		$getKey = $_GET['key'];
+if (isset($_GET['key'])) {
+$getKey = $_GET['key'];
 
-		if (isset($_GET['value'])) {
-			$getValue = $_GET['value'];
+if (isset($_GET['value'])) {
+$getValue = $_GET['value'];
 
-			//$_GET['key'] の値が登録されている場合、対象カラムを $_GET['value'] に変更する
-			$updateSql = 'UPDATE ' .$table_name;
-			$updateSql .= ' SET ' .$valueColumn_name. ' = CASE WHEN ' .$keyColumn_name. ' = ? THEN ? END';
-			$updateSql .= ' WHERE ' .$keyColumn_name. ' = "' .$getKey. '"';
+//$_GET['key'] の値が登録されている場合、対象カラムを $_GET['value'] に変更する
+$updateSql = 'UPDATE ' .$table_name;
+$updateSql .= ' SET ' .$valueColumn_name. ' = CASE WHEN ' .$keyColumn_name. ' = ? THEN ? END';
+$updateSql .= ' WHERE ' .$keyColumn_name. ' = "' .$getKey. '"';
 
-			$updateStmt = $pdo->prepare($updateSql);
-			$updateStmt->execute(array($getKey, $getValue));
-			
-			//$_GET['key'] の値が登録されていない場合、エラーを返す
-			$searchSql = 'SELECT ' .$keyColumn_name;
-			$searchSql .= ' FROM ' . $table_name;
-			$searchSql .= ' WHERE ' .$keyColumn_name. ' = "' .$getKey.'"';
+$updateStmt = $pdo->prepare($updateSql);
+$updateStmt->execute(array($getKey, $getValue));
 
-			$searchStmt = $pdo->query($searchSql);
+//$_GET['key'] の値が登録されていない場合、エラーを返す
+$searchSql = 'SELECT ' .$keyColumn_name;
+$searchSql .= ' FROM ' . $table_name;
+$searchSql .= ' WHERE ' .$keyColumn_name. ' = "' .$getKey.'"';
 
-			if (count($searchStmt->fetchAll()) == 0) {
-				echo 'ERROR! key[' .$getKey. ']は登録されていません';
-			}
+$searchStmt = $pdo->query($searchSql);
 
-		} else {
-			echo 'ERROR! valueがセットされていません';
-		}
-	} else {
-		echo 'ERROR! keyがセットされていません';
-	}
+if (count($searchStmt->fetchAll()) == 0) {
+echo 'ERROR! key[' .$getKey. ']は登録されていません';
+}
+
+} else {
+echo 'ERROR! valueがセットされていません';
+}
+} else {
+echo 'ERROR! keyがセットされていません';
+}
 
 } catch (PDOException $e) {
     exit('ERROR! データベース接続失敗' . $e->getMessage());	
